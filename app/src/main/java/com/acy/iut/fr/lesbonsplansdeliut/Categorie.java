175987a39f126adb33323bab42804181756b261f
mesagoul <lucasmesagouiffes@gmail.com>
2016-03-01 09:23:27 +0100

    Change CategorieClass

--- app/src/main/java/com/acy/iut/fr/lesbonsplansdeliut/Categorie.java
+++ app/src/main/java/com/acy/iut/fr/lesbonsplansdeliut/Categorie.java
@@ -4,20 +4,20 @@ package com.acy.iut.fr.lesbonsplansdeliut;
  * Created by boinnarr on 01/03/2016.
  */
 public class Categorie {
-    private int id_categorie;
+    private int id;
     private String nom;
 
     public Categorie(int id_categorie, String nom) {
-        this.id_categorie = id_categorie;
+        this.id = id_categorie;
         this.nom = nom;
     }
 
     public int getId_categorie() {
-        return id_categorie;
+        return id;
     }
 
-    public void setId_categorie(int id_categorie) {
-        this.id_categorie = id_categorie;
+    public void setId_categorie(int id) {
+        this.id = id;
     }
 
     public String getNom() {
