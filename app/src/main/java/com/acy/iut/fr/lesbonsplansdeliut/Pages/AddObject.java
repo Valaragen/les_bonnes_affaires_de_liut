4e1fcf35afcceb0502646b2d55ad638372ef76f2
juanpii <bidalia1100@hotmail.com>
2016-03-07 17:31:08 +0100

    Add object activity and interface

--- app/src/main/java/com/acy/iut/fr/lesbonsplansdeliut/Pages/AddObject.java
+++ app/src/main/java/com/acy/iut/fr/lesbonsplansdeliut/Pages/AddObject.java
@@ -1,18 +1,48 @@
 package com.acy.iut.fr.lesbonsplansdeliut.Pages;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.acy.iut.fr.lesbonsplansdeliut.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddObject extends Activity {
+    private static final String FLAG_SUCCESS = "success";
+    private static final String FLAG_MESSAGE = "message";
+    private static final String URL = "http://rudyboinnard.esy.es/android/";
+    private ArrayList<String> listCategories = new ArrayList<String>();
+    private Spinner spinnerCategories;
+    private EditText titreObjet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_object);
+
+        titreObjet = (EditText) findViewById(R.id.titreObjet);
+        spinnerCategories = (Spinner) findViewById(R.id.spinnerCategories);
     }
 
     @Override
@@ -35,5 +65,97 @@ public class AddObject extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    class LoadPage extends AsyncTask<String, String, JSONObject> {
+
+        //display loading and status
+        protected void onPreExecute() {
+        }
+
+        //Get JSON data from the URL
+        @TargetApi(Build.VERSION_CODES.KITKAT)
+        @Override
+        protected JSONObject doInBackground(String... args) {
+            JSONObject json = null;
+            try {
+                Log.d("request", "starting GetCategories");
+                URL url = null;
+                HttpURLConnection connection = null;
+                try {
+                    //initialize connection
+                    url = new URL(URL);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("POST");
+                    String urlParameters = "method=LoadPageAddObject";
+                    byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+                    //write post data to URL
+                    DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+                    wr.write(postData);
+                    //connect and get data
+                    connection.connect();
+                    InputStream in = new BufferedInputStream(connection.getInputStream());
+                    json = new JSONObject(convertStreamToString(in));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return json;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        //parse returned data
+        protected void onPostExecute(JSONObject result) {
+            int success = 0;
+
+            try {
+
+
+                JSONArray jArrayDept = (result.getJSONArray("nom_categorie"));
+                JSONArray jArrayid = (result.getJSONArray("id_categorie"));
+                if (jArrayDept != null) {
+                    for (int i=0;i<jArrayDept.length();i++){
+                        listCategories.add(jArrayDept.get(i).toString());
+                        Log.d("DEBUG",listCategories.get(i));
+
+                    }
+                }
+                fillSpinner(spinnerCategories,listCategories);
+                //alert the user of the status of the connection
+                success = result.getInt(FLAG_SUCCESS);
+                Toast.makeText(AddObject.this, (String) result.getString("nom_departement"),
+                        Toast.LENGTH_LONG).show();
+                //testText.setText(result.getString(FLAG_MESSAGE)+"");
+            } catch (JSONException e) {
+                Log.e("JSON Parser", "Error parsing data " + e.toString());
+                //e.printStackTrace();
+            }
+            //log the success status
+            if (success == 1) {
+                Log.d("OK", "OK");
+            } else {
+                Log.d("Error", "Error");
+            }
+        }
+    }
+
+    public String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+
+    public void fillSpinner(Spinner spinner, List<String> list) {
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        Log.d("Spinner", "Spinner adaptee");
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Log.d("Spinner", "Spinner set");
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+        Log.d("Spinner", "Spinner fini");
     }
 }
